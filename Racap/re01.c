d36e85bdf9af79531db763432cefbc7ed260d24d
pondkerz <preechaporn5866@gmail.com>
2025-12-03 01:19:13 +0700

    day 1(not success 2hr)

--- Racap/re01.c
+++ Racap/re01.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
-int main(){
-    char word[100] ;
-    getf()
+void check(char word[]){
+    int i=0, word_count=0 ;
+    while(1)
+    {
+        int count = 0;
+        while(1)
+        {
+            if ((word[i] != '\n') && (word[i] != ' '))
+            {
+                printf("%c",word[i]);
+                i++;
+                count++;
+            }
+            else
+            {
+                i++;
+                for(i;;i++)
+                {
+                    if((word[i] != ' ') && (word[i] != '\n') && (word[i] != '\0')) 
+                    {
+                        word_count++;
+                        break;
+                    }
+                }
+                printf(" (%d)\n",count);
+                break;
+            }   
+        }
+        if (word[i] == '\n')
+        {
+            if (count > 0) 
+            {
+                word_count++;
+            }
+            printf("total word = %d",word_count);
+            break;
+        }
+    }
+}
 
+int main()
+{
+    char word[200] ;
+    printf("enter something\n");
+    fgets(word,200,stdin);
+    check(word);
+    return 0;
 }
